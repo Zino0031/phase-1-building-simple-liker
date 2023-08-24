5f4aa80b3154eecf0910d2859620dc593140a18f
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-08-24 23:34:58 +0200

    Completed Assignment

--- main.js
+++ main.js
@@ -4,6 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+  const emptyHearts = document.getElementsByClassName('like-glyph');
+
+  Array.from(emptyHearts).forEach((heart) => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+      .then(()=>{
+        if(heart.textContent === EMPTY_HEART){
+          heart.textContent = FULL_HEART;
+          heart.classList.add('activated-heart');
+        }else{
+          heart.textContent = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        }
+      })
+      .catch((error) => {
+        const errorModal = document.getElementById('modal');
+        const errormsg = document.getElementById('modal-message');
+
+        errormsg.textContent = error;
+        errorModal.classList.remove('hidden');
+
+
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+
+
+      })
+    }); 
+  });
+
+
 
 
 
